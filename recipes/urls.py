4a640187b415f4362baf4f44313dc0e89b5e492f
EliabeRP <eliabe.rodrgu3s@gmail.com>
2023-09-13 18:28:39 -0300

    config static files & refactoring

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from recipes.views import home
+from . import views
+
+app_name = 'recipes'
 
 urlpatterns = [
-    path('', home),
+    path('', views.home, name='home'),
+    path('recipes/<int:id>/', views.recipe, name='recipe')
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(10)]
+    })
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True
+    })
